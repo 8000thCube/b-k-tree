db6e9ebc2f84331d2ec9e84ebad833cece4ab59b
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-05-15 09:50:09 -0700

    Update metrics.rs

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,3 +1,4 @@
+ham_int!(i128,i16,i32,i64,i8,isize,u128,u16,u32,u64,u8,usize);
 impl CeilL2{
 	/// creates a new metric that is the ceiling function of the euclidean metric
 	pub fn new()->Self{Self::with_factor(1.0)}
@@ -15,6 +16,16 @@ impl Hamming{
 		Self{_seal:()}
 	}
 }
+impl HamIter for String{
+	fn ham_iter(&self)->Self::Iter<'_>{self.chars()}
+	type Item<'a>=char;
+	type Iter<'a>=Chars<'a> where Self:'a;
+}
+impl HamIter for str{
+	fn ham_iter(&self)->Self::Iter<'_>{self.chars()}
+	type Item<'a>=char;
+	type Iter<'a>=Chars<'a> where Self:'a;
+}
 impl LevIter for String{
 	fn lev_iter(&self)->Self::Iter<'_>{self.chars()}
 	type Item<'a>=char;
@@ -31,6 +42,24 @@ impl Levenshtein{
 		Self{cache:Arc::new(Mutex::new(Vec::new()))}
 	}
 }
+impl<D:Add<Output=D>+Copy+Into<f64>+Mul<Output=D>,E,const N:usize> CoordIter for [E;N] where for<'a>&'a E:Sub<Output=D>{
+	fn coord_iter(&self)->Self::Iter<'_>{self.iter()}
+	type D=D;
+	type Item<'a>=&'a E where E:'a;
+	type Iter<'a>=SliceIter<'a,E> where Self:'a;
+}
+impl<D:Add<Output=D>+Copy+Into<f64>+Mul<Output=D>,E> CoordIter for [E] where for<'a>&'a E:Sub<Output=D>{
+	fn coord_iter(&self)->Self::Iter<'_>{self.iter()}
+	type D=D;
+	type Item<'a>=&'a E where E:'a;
+	type Iter<'a>=SliceIter<'a,E> where Self:'a;
+}
+impl<D:Add<Output=D>+Copy+Into<f64>+Mul<Output=D>,E> CoordIter for Vec<E> where for<'a>&'a E:Sub<Output=D>{
+	fn coord_iter(&self)->Self::Iter<'_>{self.iter()}
+	type D=D;
+	type Item<'a>=&'a E where E:'a;
+	type Iter<'a>=SliceIter<'a,E> where Self:'a;
+}
 impl<E:Eq,const N:usize> LevIter for [E;N]{
 	fn lev_iter(&self)->Self::Iter<'_>{self.iter()}
 	type Item<'a>=&'a E where E:'a;
@@ -46,11 +75,17 @@ impl<E:Eq> LevIter for Vec<E>{
 	type Item<'a>=&'a E where E:'a;
 	type Iter<'a>=SliceIter<'a,E> where Self:'a;
 }
-impl<F:Add<Output=F>+Copy+Into<f64>+Mul<Output=F>,V:?Sized> DiscreteMetric<V> for CeilL2 where for<'a><&'a V as IntoIterator>::Item:Sub<Output=F>,for<'a>&'a V:IntoIterator{
-	fn distance(&self,x:&V,y:&V)->usize{
-		let (mut x,mut y)=(x.into_iter(),y.into_iter());
-		let factor=self.factor;
+impl<T:?Sized+CoordIter> CoordIter for &T{
+	fn coord_iter(&self)->Self::Iter<'_>{(*self).coord_iter()}
+	type D=T::D;
+	type Item<'a>=T::Item<'a> where Self:'a;
+	type Iter<'a>=T::Iter<'a> where Self:'a;
+}
+impl<T:?Sized+CoordIter> CeilL2Distance for T{
+	fn ceil_l2_distance(&self,factor:f64,other:&Self)->usize{
+		let (mut x,mut y)=(self.coord_iter(),other.coord_iter());
 		let mut r2=None;
+
 		loop{
 			let d=if let (Some(x),Some(y))=(x.next(),y.next()){x-y}else{break};
 			let d2=d*d;
@@ -59,6 +94,31 @@ impl<F:Add<Output=F>+Copy+Into<f64>+Mul<Output=F>,V:?Sized> DiscreteMetric<V> fo
 		let r2=r2.map(Into::into).unwrap_or(0.0);
 		((r2.sqrt()/factor).ceil()*factor) as usize
 	}
+}
+impl<T:?Sized+HamIter> HamIter for &T{
+	fn ham_iter(&self)->Self::Iter<'_>{(*self).ham_iter()}
+	type Item<'a>=T::Item<'a> where Self:'a;
+	type Iter<'a>=T::Iter<'a> where Self:'a;
+}
+impl<T:?Sized+HamIter> HammingDistance for T{
+	fn hamming_distance(&self,other:&Self)->usize{
+		let (mut x,mut y)=(self.ham_iter(),other.ham_iter());
+		let mut count=0;
+
+		loop {
+			match (x.next(),y.next()){
+				(Some(a),Some(b))=>if a!=b{count+=1},
+				(Some(_), None) | (None, Some(_))=>count+=1,
+				(None, None)=>break,
+			}
+		}
+		count
+	}
+}
+impl<T:?Sized+LevIter> LevIter for &T{
+	fn lev_iter(&self)->Self::Iter<'_>{(*self).lev_iter()}
+	type Item<'a>=T::Item<'a> where Self:'a;
+	type Iter<'a>=T::Iter<'a> where Self:'a;
 }
 impl<T:?Sized+LevIter> LevenshteinDistance for T{
 	fn levenshtein_distance(&self,distances:&mut Vec<usize>,other:&Self)->usize{
@@ -83,6 +143,12 @@ impl<T:?Sized+LevIter> LevenshteinDistance for T{
 	}
 	fn levenshtein_len(&self)->usize{self.lev_iter().count()}
 }
+impl<V:CeilL2Distance+?Sized> DiscreteMetric<V> for CeilL2{
+	fn distance(&self,x:&V,y:&V)->usize{x.ceil_l2_distance(self.factor,y)}
+}
+impl<V:HammingDistance+?Sized> DiscreteMetric<V> for Hamming{
+	fn distance(&self,x:&V,y:&V)->usize{x.hamming_distance(y)}
+}
 impl<V:LevenshteinDistance+?Sized> DiscreteMetric<V> for Levenshtein{
 	fn distance(&self,x:&V,y:&V)->usize{
 		let (mut x,mut y)=(x,y);
@@ -102,8 +168,42 @@ impl<V:LevenshteinDistance+?Sized> DiscreteMetric<V> for Levenshtein{
 		distance
 	}
 }
+/// implements hamming distance for integers
+macro_rules! ham_int{
+	($($int:ty),*)=>($(
+		impl HammingDistance for &$int{
+			fn hamming_distance(&self,other:&Self)->usize{(*self^*other).count_ones() as usize}
+		}
+		impl HammingDistance for $int{
+			fn hamming_distance(&self,other:&Self)->usize{(self^other).count_ones() as usize}
+		}
+	)*)
+}
 #[cfg(test)]
 mod tests{
+	#[test]
+	fn ceil_l2_2d_int(){
+		let m=CeilL2::new();
+		assert_eq!(m.distance(&[1,2],&[2,1]),2);
+		assert_eq!(m.distance(&[3,3],&[3,4]),1);
+		assert_eq!(m.distance(&[0,0],&[3,4]),5);
+	}
+	#[test]
+	fn ham_int(){
+		let m=Hamming::new();
+		assert_eq!(m.distance(&1,&0),1);
+		assert_eq!(m.distance(&0b1011101,&0b1001001),2);
+		assert_eq!(m.distance(&9999,&9999),0);
+		assert_eq!(m.distance(&-1_i32,&0_i32),32);
+	}
+	#[test]
+	fn ham_string() {
+		let m=Hamming::new();
+		assert_eq!(m.distance("rust","rust"),0);
+		assert_eq!(m.distance("karolin","kathrin"),3);
+		assert_eq!(m.distance("1011101","1001001"),2);
+		assert_eq!(m.distance("2173896","2233796"),3);
+	}
 	#[test]
 	fn lev(){
 		let metric=Levenshtein::new();
@@ -126,24 +226,55 @@ pub struct Hamming{_seal:()}
 #[derive(Clone,Debug,Default)]
 /// levenshtein distance metric for strings
 pub struct Levenshtein{cache:Arc<Mutex<Vec<usize>>>}
-/// trait for levenshtein distance compatibility
-pub trait LevenshteinDistance{
-	/// computes the distance given and other string its initial distances to substrings of self
-	fn levenshtein_distance(&self,distances:&mut Vec<usize>,other:&Self)->usize;
-	/// returns the string length to be used for lev distance purposes
-	fn levenshtein_len(&self)->usize;
+/// trait for computing coordinate based distances by iteration over primitives
+pub trait CoordIter{
+	/// returns an iterator over coordinates
+	fn coord_iter(&self)->Self::Iter<'_>;
+	/// the coordinate displacement type
+	type D:Add<Output=Self::D>+Copy+Into<f64>+Mul<Output=Self::D>;
+	/// the item type
+	type Item<'a>:Sub<Output=Self::D> where Self:'a;
+	/// the iterator type
+	type Iter<'a>:Iterator<Item=Self::Item<'a>> where Self:'a;
+}
+/// trait for computing the hamming distance by iteration
+pub trait HamIter{
+	/// returns an iterator for comparing characters in a hamming distance
+	fn ham_iter(&self)->Self::Iter<'_>;
+	/// the item type
+	type Item<'a>:Eq where Self:'a;
+	/// the iterator type
+	type Iter<'a>:Iterator<Item=Self::Item<'a>> where Self:'a;
+}
+/// trait for ceil l2 distance compatibility
+pub trait CeilL2Distance{
+	/// computes the ceiling l2 distance
+	fn ceil_l2_distance(&self,factor:f64,other:&Self)->usize;
+}
+/// trait for hamming distance compatibility
+pub trait HammingDistance{
+	/// computes the distance given and other string
+	fn hamming_distance(&self,other:&Self)->usize;
 }
 /// trait for computing levenshtein distance by iteration
 pub trait LevIter{
-	/// returns an iterator for comparing characters in a levenshtein distance. It shouldn't change length or sequence without self being mutated
+	/// returns an iterator for comparing characters in a levenshtein distance. Since this distance requires multiple iterations, it shouldn't change length or sequence without self being mutated
 	fn lev_iter(&self)->Self::Iter<'_>;
 	/// the item type
 	type Item<'a>:Eq where Self:'a;
 	/// the iterator type
 	type Iter<'a>:Iterator<Item=Self::Item<'a>> where Self:'a;
 }
+/// trait for levenshtein distance compatibility
+pub trait LevenshteinDistance{
+	/// computes the distance between self and other string given its initial distances to substrings of self
+	fn levenshtein_distance(&self,distances:&mut Vec<usize>,other:&Self)->usize;
+	/// returns the string length to be used for levenshtein distance purposes
+	fn levenshtein_len(&self)->usize;
+}
 use {
 	crate::DiscreteMetric,
+	ham_int,
 	std::{
 		cmp::Eq,mem::{swap,take},ops::{Add,Mul,Sub},slice::Iter as SliceIter,str::Chars,sync::{Arc,Mutex}
 	}
