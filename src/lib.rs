ccaabd47dfc44d5016b266db81154adb32363e89
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-05-15 16:46:18 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 mod tree;
 pub mod metrics;
-pub use tree::{BKTreeMap,BKTreeSet,DiscreteMetric};
+pub use tree::{BKTreeMap,BKTreeSet,CloseKeyIter,CloseMapIter,CloseSetIter,CloseValIter,DiscreteMetric,KeyIter,MapIter,SetIter,ValIter};
